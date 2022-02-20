76b8e13b672f3521ba4a9bbdd9f106e8151782e6
turinglabsorg <sebastiano.cataudo@gmail.com>
2022-02-20 17:21:54 +0100

    removing useless 1155

--- vesting/contracts/CaratoVesting.sol
+++ vesting/contracts/CaratoVesting.sol
@@ -2,28 +2,52 @@
 pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "../../erc20/IERC20.sol";
-import "../../erc1155/IERC1155.sol";
+import "../interfaces/IERC20.sol";
+import "../interfaces/IERC1155.sol";
 
 contract CaratoVesting is Ownable {
-    IERC1155 private Carato1155;
+    IERC1155 private MEGO;
     IERC20 private Carato20;
     mapping(uint256 => mapping(address => bool)) public _tokensClaimed;
-    event Claimed(address indexed _by, uint256 indexed _id_event, uint256 _value);
+    mapping(uint256 => uint256) public _approved;
+    event Claimed(
+        address indexed _by,
+        uint256 indexed _id_event,
+        uint256 _value
+    );
+    address _daoAddress;
+
+    function selectDaoAddress(address _address) public onlyOwner {
+        _daoAddress = _address;
+    }
 
     function selectErc20(address _address) public onlyOwner {
         Carato20 = IERC20(_address);
     }
 
     function selectErc1155(address _address) public onlyOwner {
-        Carato1155 = IERC1155(_address);
+        MEGO = IERC1155(_address);
+    }
+
+    function approveEvent(uint256 _id_event, uint256 _value) public {
+        require(
+            msg.sender == _daoAddrerss,
+            "CaratoVesting: Only Dao can approve events"
+        );
+        _approved[_id_event] = _value;
     }
 
     function claimTokens(uint256 _id_event) public {
-        uint256 value = Carato1155.returnEventValue(_id_event);
+        uint256 value = _approved[_id_event];
         require(value > 0, "CaratoVesting: Event doesn't exists");
-        require(_tokensClaimed[_id_event][msg.sender] == false, "CaratoVesting: You already claimed your tokens");
-        require(Carato1155.balanceOf(msg.sender, _id_event) > 0, "CaratoVesting: You must own the 1155 token");
+        require(
+            _tokensClaimed[_id_event][msg.sender] == false,
+            "CaratoVesting: You already claimed your tokens"
+        );
+        require(
+            Carato1155.balanceOf(msg.sender, _id_event) > 0,
+            "CaratoVesting: You must own the 1155 token"
+        );
         _tokensClaimed[_id_event][msg.sender] = true;
         Carato20.mint(msg.sender, value);
         emit Claimed(msg.sender, _id_event, value);
